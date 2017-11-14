16bc56e3698a60610ea8f79406266fd8c12a55ab
cody <wangkedi.wang@gmail.com>
2017-11-14 20:23:51 +0800

    UPDATE Version 1.0.1

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ const router = new Router();
 
 wrapper(router);
 
-router.swagger({ title: 'Server', description: 'API DOC', version: '1.0.0' });
+router.swagger({ swaggerHtmlEndpoint: '/swagger-html', swaggerJsonEndpoint: 'swagger-json', title: 'Server', description: 'API DOC', version: '1.0.0' });
 
 router.map(SampleRouter);
 router.map(UserRouter);
